75dc5f7b39a95a7e4c09f848b92affc05f6107e4
Mohamud <msheeq99@outlook.com>
2022-10-12 17:12:05 +0100

    Put comments on the script based on the acceptance criteria

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   console.log("Hey, You clicked the button!")
+  
+// 1. Prompt the user for the password criteria .
+//    a. Password length 8 < 128.
+
+//    b. lowercase, uppercase, numbers, special characters.
+
+
+// 2. Validate the input.
+
+// 3. Generate password based on the criteria.
 
   
   
   
   
-  
-  
+// 4. Display password to the page.   
   return "Generate password will go here!";
 }
 
