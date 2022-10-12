446637b825f47f484736e243b0672ba72c9d5758
Mohamud <msheeq99@outlook.com>
2022-10-12 20:44:51 +0100

    comments based on critera

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,29 @@
 // Assignment Code
+
+// lower, uppercase, numbers, symbols and password.
 const generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
-  console.log("Hey, You clicked the button!");
-  
-// 1. Prompt the user for the password criteria .
-      
-
-//    a. Password length 8 < 128.
-  const length = document.querySelector("#length") 
-
-// 2. Validate the input.
-const incNumbers = document.querySelector("#numbers");
-const passwordText = document.querySelector("#specialCharacters");
- 
-
-//    b. lowercase, uppercase, numbers, special characters.
-  const lowercase = "abcdefghijklmnopqrstuvwxyz";
-  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numbers = "0123456789";
-  const specialCharacters = "!@#$%^£&*_-+=";
-
-
-// 3. Generate password based on the criteria.
-
-  
-  
-  
-  
-
-}
-
+const numbers = ("0123456789");
+const symbols = ("!£$%^&*@~#+=_-");
+const lowerCase = ("abcdefghijklmnopqrstuvwxyz");
+const upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+const password = ("");
 // Write password to the #password input
-
-
+// Validate the input.
 function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  var passwordLength = numbers;
 
   passwordText.value = password;
-
 }
+// Generate password based on criteria.
+function generatePassword() {
+  console.log("Hey, you clicked the button!")
+  let passwordLength = prompt ("Do you want the password to have a certain length? ")
+} 
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 4. Display password to the page.   
-  return "Generate password will go here!";
