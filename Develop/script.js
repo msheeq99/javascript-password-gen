bdebdf8160706573b7e31fea39664bf46b813591
Mohamud <msheeq99@outlook.com>
2022-10-17 18:38:43 +0100

    in the console its still coming back as undefined, still trying to figure out the issue

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,11 @@
 
 // lower, uppercase, numbers, symbols and password.
 const generateBtn = document.querySelector("#generate");
-const numbers = ("0123456789");
-const symbols = ("!£$%^&*@~#+=_-");
-const lowerCase = ("abcdefghijklmnopqrstuvwxyz");
-const upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-const password = ("");
+const numbers = ["0123456789"];
+const symbols = ["!£$%^&*@~#+=_-"];
+const lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+const upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+const password = [""];
 // Write password to the #password input
 // Validate the input.
 let  charsMixed = [];
@@ -27,7 +27,7 @@ function generatePassword() {
 
   let passwordUpperCase = prompt("Would you like to include uppercase character?");
   if (passwordUpperCase === "yes") {
-    charsMixed = passwordUpperCase.concat(passwordUpperCase);
+    charsMixed = charsMixed.concat(passwordUpperCase);
     window.alert("Uppercase added !");
   }
 
@@ -37,49 +37,46 @@ function generatePassword() {
 
   let passwordLowerCase = prompt("Would you like to include lowercase character?")
   if (passwordLowerCase === "yes") {
-    charsMixed = passwordLowerCase.concat(passwordLowerCase);
-    window.alert("Lowercase added !")
+    charsMixed = charsMixed.concat(passwordLowerCase);
+    window.alert("Lowercase added !");
   }
 
   if (passwordLowerCase === "no") {
-    window.alert("Lowercase dismissed !")
+    window.alert("Lowercase dismissed !");
   } 
 
   let passwordSymbols = prompt("Would like to include symbol?")
   if (passwordSymbols === "yes") {
-    charsMixed = passwordSymbols.concat(passwordSymbols);
-    window.alert("Symbols added !")
+    charsMixed = charsMixed.concat(passwordSymbols);
+    window.alert("Symbols added !");
   }
 
   if (passwordSymbols === "no") {
-    window.alert("Symbols dismissed !")
+    window.alert("Symbols dismissed !");
   }
 
   let passwordNumbers = prompt("Would like to include numbers?")
   if (passwordNumbers === "yes") {
-    charsMixed = passwordNumbers.concat(passwordNumbers);
-    window.alert("Numbers added !")
+    charsMixed = charsMixed.concat(passwordNumbers);
+    window.alert("Numbers added !");
   }
 
   if (passwordSymbols === "no") {
-    window.alert("Numbers dismissed !")
+    window.alert("Numbers dismissed !");
   }
 
-  
-let password = "";
-for (let i = 0; i <passwordLength; i++) {
-password + i + "";
-var randomCharsMixed = Math.floor(Math.random() * charsMixed.Length);
-var selectedCharsMixed = charsMixed[randomCharsMixed];
-password = password + selectedCharsMixed;
+console.log(charsMixed);
 
-}
-console.log(password);
-alert(password);
+var password = ""
 
+for (let i = 0, n = charsMixed.length; i <parseInt(pw); i++) {
+  password += charsMixed[Math.floor.apply(Math.random() * n)]
+} 
 
 return password;
+
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
